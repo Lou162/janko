f057bf43a5da741f0718b4ddf5f98066e5c5c693
Lou162 <louna.libbrecht@gmail.com>
2025-10-05 16:53:17 +0200

    protect the sensible data

--- src/configs/fireBaseConfigs.ts
+++ src/configs/fireBaseConfigs.ts
@@ -1,20 +1,23 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, setDoc, doc } from "firebase/firestore/lite";
+import { getFirestore, setDoc, doc, collection } from "firebase/firestore/lite";
 import type { Room } from "../models/roomModel";
 
 // TODO: Replace the following with your app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB2qCEw3KxKOaPfG-8Kg38MJwUUUQV_GIY",
-  authDomain: "janko-50553.firebaseapp.com",
-  projectId: "janko-50553",
-  storageBucket: "janko-50553.firebasestorage.app",
-  messagingSenderId: "858392337646",
-  appId: "1:858392337646:web:daed37426dadeb3b2427ff",
-  measurementId: "G-QEHCH5DCWK",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  // databaseURL:
+  //   "https://janko-cd115-default-rtdb.europe-west1.firebasedatabase.app",
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const roomsRef = collection(db, "rooms");
 
 // Get a list of cities from your database
 // async function getCities(db) {
@@ -28,6 +31,8 @@ const db = getFirestore(app);
 // }
 
 async function addRoom(data: Room, name: string) {
-  await setDoc(doc(db, "Rooms", name), data);
+  console.log(import.meta.env.VITE_API_KEY);
+  console.log("Adding room to Firestore:", data);
+  await setDoc(doc(roomsRef, name), data);
 }
 export { db, addRoom };
--- src/pages/Home/App.tsx
+++ src/pages/Home/App.tsx
@@ -2,10 +2,13 @@ import "../../App.css";
 import MenuBouton from "../../components/menuBouton/menuBouton";
 
 function App() {
+  console.log("Environment variable:", import.meta.env.MODE);
+  console.log("Environment variable:", import.meta.env.VITE_SOME_KEY);
+  console.log("Environment variable:", import.meta.env.VITE_TEST);
   return (
     <>
       <div className='containerApp'>
-        <h1 className='titleApp'>JANKO {import.meta.env.TEST}</h1>
+        <h1 className='titleApp'>JANKO</h1>
 
         {/* <h1>Welcome to Janko</h1> */}
         <div className='menuApp'>
